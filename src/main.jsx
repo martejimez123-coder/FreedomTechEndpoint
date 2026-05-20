a684257f1ccfb8caeb1eaca5978fc79c6f538979
martejimez123-coder <martejimez123@gmail.com>
2026-05-19 23:03:27 -0400

    Add Vite config and update dependencies for React 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import FreedomTechWebsite from "./freedom_tech_website.jsx";
 import "./styles.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FreedomTechWebsite />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  server: {
+    host: '127.0.0.1',
+    port: 5173
+  }
+}
