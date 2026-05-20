a5c3a1211c2cffebf54e00635422a1e070ce542f
martejimez123-coder <martejimez123@gmail.com>
2026-05-19 22:58:18 -0400

    Update main.jsx with correct component import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FreedomTechWebsite from "../freedom_tech_website.jsx";
+import FreedomTechWebsite from "./freedom_tech_website.jsx";
 import "./styles.css";
 
 ReactDOM.render(
